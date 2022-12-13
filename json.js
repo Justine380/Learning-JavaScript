4b4bbdbe0d3aa6f0bd8ea9ff8cbc096fd0ea56e2
NabaalaJustine <justinenabaala@gmail.com>
2022-12-13 11:55:07 +0300

    json file in js

---
+++ json.js
@@ -0,0 +1,14 @@
+[
+    {
+      "name": "Big Corporation", 
+      "numberOfEmployees": 10000,
+      "CEO":"Justine",
+      "rating": 3.9 
+    },
+    {
+        "name":"Small Startup",
+        "numberOfEmployees": 17,
+        "CEO":null,
+        "rating": 4.5   
+    }
+]
